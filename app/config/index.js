9b9c088beefa95d624ef82bc9536ecb1e6b660ef
patgod85 <patgod85@gmail.com>
2015-05-29 11:30:33 +0300

    using of NODE_ENV in configuration

--- app/config/index.js
+++ app/config/index.js
@@ -1,8 +1,10 @@
 var nconf = require('nconf');
 var path = require('path');
 
+var node_env = process.env.NODE_ENV || 'demo';
+
 nconf.argv()
     .env()
-    .file({file: path.join(__dirname, 'config.json')});
+    .file({file: path.join(__dirname, 'config.' + node_env + '.json')});
 
 module.exports = nconf;
